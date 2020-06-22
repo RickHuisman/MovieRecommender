c8a47cdf181cf93d74bd3f05c1b1defc0aaf9140
rickhuis <rick.huisman@live.nl>
2020-06-22 18:00:22 +0200

    Get movies from csv file

---
+++ MovieRecommender/Movie.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using CsvHelper.Configuration.Attributes;
+
+namespace MovieRecommender
+{
+    public class Movie
+    {
+        [Index(0)]
+        public int ReleaseYear { get; set; }
+        [Index(1)]
+        public string Title { get; set; }
+        [Index(2)]
+        public string Origin { get; set; }
+        [Index(3)]
+        public string Director { get; set; }
+        [Index(4)]
+        public string Cast { get; set; }
+        [Index(5)]
+        public string WikiPage { get; set; }
+        [Index(6)]
+        public string Plot { get; set; }
+    }
+}
--- MovieRecommender/Program.cs
+++ MovieRecommender/Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using CsvHelper;
 
 namespace MovieRecommender
 {
     class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            var movies = GetMovies();
+            foreach (var movie in movies)
+            {
+                Console.WriteLine(movie.Title);
+            }
+        }
+
+        private static List<Movie> GetMovies()
+        {
+            using var streamReader = new StreamReader("/Users/rickhuisman/Downloads/wiki_movie_plots_deduped.csv");
+            using var csvReader = new CsvReader(streamReader, CultureInfo.InvariantCulture);
+
+            return csvReader.GetRecords<Movie>().ToList();
         }
     }
 }
